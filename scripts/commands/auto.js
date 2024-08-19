962daa5f63b1eef8d6d7ad847a2b37393fe1f937
MAHABUB RAHMAN <sadiyaxmahabur@gmail.com>
2024-08-19 08:16:36 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -1,1 +1,40 @@
-.....
+
+module.exports = {
+  config:{
+    name: "auto",
+    version: "0.0.2",
+    permission: 0,
+    prefix: true,
+    credits: "Nayan",
+    description: "auto video download",
+    category: "user",
+    usages: "",
+    cooldowns: 5,
+},
+start: async function({ nayan, events, args }) {},
+handleEvent: async function ({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const content = event.body ? event.body : '𝑃𝐿𝐸𝐴𝑆𝐸 𝑊8 𝐵𝐵𝑌😘';
+  const body = content.toLowerCase();
+  const {alldown} = require("nayan-media-downloader")
+  if (body.startsWith("https://")) {
+  api.setMessageReaction("♻️",                      event.messageID, (err) => {}, true);
+const data = await alldown(content);
+  console.log(data)
+  const {low, high, title} = data.data;
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+  const video = (await axios.get(high, {
+      responseType: "arraybuffer",
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
+
+        return api.sendMessage({
+            body: `《𝐓𝐈𝐓𝐋𝐄》: ${title} \n\n𝙴𝙽𝙹𝙾𝚈➪ 𝚂𝚞𝚖𝚞_𝙱𝙾𝚃`,
+            attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
+
+        }, event.threadID, event.messageID);
+    }
+}
+}
